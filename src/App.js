7b60be2d2a674d80fd2b17b81aba9265a4027e73
Samarth Parmeshwar Ikkalaki <samarthikkalaki123@gmail.com>
2024-11-05 03:20:11 +0530

    #Removed Private Route and Sign In and Sign Up

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React from "react";
-import './App.css';
-import { Routes , Route } from "react-router-dom";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
-import MainLayout from './Components/Layout/MainLayout';
+import MainLayout from "./Components/Layout/MainLayout";
 import SignInPage from "./Pages/SignInPage";
 import SignUpPage from "./Pages/SignUpPage";
 import ProfilePage from "./Pages/ProfilePage";
 import { PrivateRoute } from "./Routing/PrivateRoute";
 
-const App = () => { 
-    return (
-      <div>
-        <Routes>
-          <Route exact path="/" element={<MainLayout />}>
-            <Route
-              exact
-              index
-              element={
-                <PrivateRoute>
-                  <HomePage />
-                </PrivateRoute>
-              }
-            />
-            <Route exact path="/sign-in-page" element={<SignInPage />} />
-            <Route exact path="/sign-up-page" element={<SignUpPage />} />
-          </Route>
-        </Routes>
-      </div>
-    );
-}
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/" element={<MainLayout />}>
+          <Route exact index element={<HomePage />} />
+          {/* <Route exact path="/sign-in-page" element={<SignInPage />} />
+          <Route exact path="/sign-up-page" element={<SignUpPage />} /> */}
+        </Route>
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
